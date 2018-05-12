6f03cb4f711fa2b9d07eb983d13f1e6fa93d58a8
Elisa Urda <urda.elisa@gmail.com>
2018-05-12 09:47:45 -0400

    added more orm

--- config/orm.js
+++ config/orm.js
@@ -12,45 +12,50 @@ var connection = require("./connection.js");
 // The above helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
 // ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
-    var arr = [];
-  
-    for (var i = 0; i < num; i++) {
-      arr.push("?");
-    }
-  
-    return arr.toString();
-  }
-  
-  // Helper function to convert object key/value pairs to SQL syntax
-  function objToSql(ob) {
-    var arr = [];
-  
-    // loop through the keys and push the key/value as a string int arr
-    for (var key in ob) {
-      var value = ob[key];
-      // check to skip hidden properties
-      if (Object.hasOwnProperty.call(ob, key)) {
-        // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
-        if (typeof value === "string" && value.indexOf(" ") >= 0) {
-          value = "'" + value + "'";
-        }
-        // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
-        // e.g. {sleepy: true} => ["sleepy=true"]
-        arr.push(key + "=" + value);
-      }
-    }
-  
-    // translate array of strings to a single comma-separated string
-    return arr.toString();
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
   }
 
-  var orm = {
-      all: function (tableInput){
-        var queryString = "SELECT * FROM" + tableInput;
-        connection.query(queryString, function (err, res) {
-            if (err) {
-                throw err
-            }
-        })
+  return arr.toString();
+}
+
+// Helper function to convert object key/value pairs to SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // loop through the keys and push the key/value as a string int arr
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
       }
+      // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+      // e.g. {sleepy: true} => ["sleepy=true"]
+      arr.push(key + "=" + value);
+    }
   }
+
+  // translate array of strings to a single comma-separated string
+  return arr.toString();
+}
+
+var orm = {
+  selectAll: function (tableInput, cb) {
+    var queryString = "SELECT * FROM" + tableInput;
+    connection.query(queryString, function (err, res) {
+      if (err) {
+        throw err
+      }
+      cb(result);
+    });
+  },
+
+  insertOne: function (tableInput, columns, values, cb) {
+    var queryString = "INSERT INTO <tablename> (columns) VALUES (values)"
+  },
+}
